9c3c509c670ddd3bf841c360d43701201f8a0497
Gabriel Diamantino <gabrielsdiamantino@hotmail.com>
2022-01-01 18:03:03 -0300

    praticando Object e ToStringAdicionar arquivos de projeto.

---
+++ Estoque/Produto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estoque
+{
+    internal class Produto
+    {
+
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade = Quantidade + quantidade;
+        }
+
+        public void SubProdutos(int quantidade)
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome 
+                + ", R$" 
+                + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", "
+                + Quantidade 
+                + " unidades, Total: R$"
+                + ValorTotalEmEstoque().ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Estoque/Program.cs
@@ -0,0 +1,57 @@
+﻿
+using System.Globalization;
+
+namespace Estoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto1 = new Produto();
+
+            Console.WriteLine("Cadastre o primeiro produto");
+            Console.Write("Nome do produto: ");
+            produto1.Nome = Console.ReadLine();
+            Console.Write("Quantidade: ");
+            produto1.Quantidade = int.Parse(Console.ReadLine());
+            Console.Write("Preço: ");
+            produto1.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Console.Write("Informações do Produto: " + produto1.ToString());
+
+            Console.Write("Digite a quantidade de produtos que deseja adicionar: ");
+            int adcProduto = int.Parse(Console.ReadLine());
+            produto1.AdicionarProdutos(adcProduto);
+
+            Console.Write("Digite a quantidade de produtos que deseja subtrair: ");
+            int subtrair = int.Parse(Console.ReadLine());
+            produto1.SubProdutos(subtrair);
+
+            Console.Write("Informações do Produto: " + produto1.ToString());
+
+            
+            Produto produto2 = new Produto();
+
+            Console.WriteLine("Cadastre o segundo produto");
+            Console.WriteLine("Nome do produto: ");
+            produto1.Nome = Console.ReadLine();
+            Console.WriteLine("Quantidade: ");
+            produto1.Quantidade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Preço: ");
+            produto1.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Informações do Produto: " + produto1.ToString());
+
+            Console.WriteLine("Digite a quantidade de produtos que deseja adicionar: ");
+            int adcProduto2 = int.Parse(Console.ReadLine());
+            produto1.AdicionarProdutos(adcProduto2);
+
+            Console.WriteLine("Digite a quantidade de produtos que deseja subtrair: ");
+            int subtrair2 = int.Parse(Console.ReadLine());
+
+            produto2.SubProdutos(subtrair2);
+
+            Console.WriteLine("Informações do Produto: " + produto2.ToString());
+        }
+    }
+}
